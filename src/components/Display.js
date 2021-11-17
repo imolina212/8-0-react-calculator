94eb499abd7f2acf07373a484f1f97b7e214fb86
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-11-16 22:18:33 -0500

    initial commit

---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Display extends React.Component {
+    render() {
+        return (
+            <div className="display">
+                <p>{this.props.display}</p>
+            </div>
+        )
+    }
+}
+
+export default Display;
